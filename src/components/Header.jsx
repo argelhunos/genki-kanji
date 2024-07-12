f7e8e72fecff2e1777340441a2a2a0d84952527c
argelhunos <52228971+argelhunos@users.noreply.github.com>
2024-07-11 22:03:55 -0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import SearchIcon from '@mui/icons-material/Search';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p>Genki Kanji</p>
+            <SearchIcon />
+        </div>
+    )
+}
+
+export default Header
